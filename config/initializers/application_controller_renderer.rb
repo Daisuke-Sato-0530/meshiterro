1a6736d25a6895a8ee078dee3c3604b779eaa7b9
Daisuke-Sato-0530 <daisuke19940530@gmail.com>
2021-01-14 10:01:38 +0000

    [start]4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '793936eb0a5ff022b318a6495991e000aff8bea54ca88058474d13d79556fadb5414fb6bbaa255cb35c40e434dd8cbb9bb79ef29bf5c278a613e74d1bb4469c8'
